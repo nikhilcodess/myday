e04b723f82fe3b62d3161a8f0275c4d5deb7b363
nikhilcodess <nikhilkaushal0000@gmail.com>
2021-05-27 11:54:40 +0530

    Added authentication with google and error handling and created models, schemas

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const connectDB = require("./config/db");
 
 //load config
 dotenv.config({ path: "./config/config.env" });
+
+//passport config
+require("./config/passport")(passport);
 
 connectDB();
 
@@ -21,11 +28,29 @@ if (process.env.NODE_ENV === "development") {
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", ".hbs");
 
+//sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongoUrl: mongoose.connection._connectionString,
+      mongoOptions: {},
+    }),
+  })
+);
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //Static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
 app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(
